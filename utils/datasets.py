ac19a85059aed18d46e6288b2b2356876544f216
whr819987540 <819987540@qq.com>
2023-09-05 10:49:01 +0800

    read from VOC2005 dataset

--- utils/datasets.py
+++ utils/datasets.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 from utils.augmentations import horisontal_flip
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
+import re
 
 
 def pad_to_square(img, pad_value):
@@ -56,13 +57,62 @@ class ImageFolder(Dataset):
         return len(self.files)
 
 
+class VOCDataset(Dataset):
+    def __init__(self, dataset_path):
+        """
+            for training, dataset_path is VOC2005_1/data/train
+            for testing, dataset_path is VOC2005_1/data/test
+        """
+        self.dataset_path = dataset_path
+        self.img_files = os.listdir(dataset_path)
+        self.label_files = []
+        for img_file in self.img_files:
+            split_path = img_file.split(".")
+            path = os.path.join(split_path[0], split_path[1], ".txt")
+            self.label_files.append(path)
+
+    def __getitem__(self, index):
+        """
+            img_path: str
+            img: PIL.
+            targets: 
+        """
+        img_path = os.path.join(self.dataset_path, self.img_files[index % len(self.img_files)])
+        img = transforms.ToTensor()(Image.open(img_path).convert('RGB'))
+
+        label_path = os.path.join("VOC2005_1/Annotations", self.label_files[index % len(self.img_files)].rstrip())
+
+        targets = []
+        # label_path对应的文件中有图片size与box size，需要你自己调整一下，这里只是简单的读取
+        with open(label_path,'r') as f:
+            content = "".join(f.readlines())
+
+            img_size_pattern = r"Image size \(X x Y x C\) : (\d+) x (\d+) x (\d+)"
+            res = re.search(img_size_pattern,content)
+            if res:
+                img_size = res.groups()
+            else:
+                raise ValueError
+            print(img_size)
+
+            # (Xmin, Ymin) - (Xmax, Ymax) : (41, 40) - (142, 113)
+            box_size_pattern = r"\(Xmin, Ymin\) - \(Xmax, Ymax\) : \((\d+), (\d+)\) - \((\d+), (\d+)\)"
+            box_size = re.findall(box_size_pattern,content)
+            
+            
+        
+        
+        return img_path, img, targets
+
+
 class ListDataset(Dataset):
     def __init__(self, list_path, img_size=416, augment=True, multiscale=True, normalized_labels=True):
         with open(list_path, "r") as file:
             self.img_files = file.readlines()
 
         self.label_files = [
-            path.replace("images", "labels").replace("JPEGImages", "labels").replace(".png", ".txt").replace(".jpg", ".txt")
+            path.replace("images", "labels").replace("JPEGImages", "labels").replace(
+                ".png", ".txt").replace(".jpg", ".txt")
             for path in self.img_files
         ]
         self.img_size = img_size
